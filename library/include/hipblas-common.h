6b7748e2fd2d902b45258887856db0085fb51b63
KKyang <YANGWEN.HUANG@AMD.COM>
2024-10-08 22:56:43 +0800

    Update hipblas-common.h
    [ROCm/hipBLAS-common commit: 3dae9305e442b47f23cb580de831e8d71d10e333]

--- library/include/hipblas-common.h
+++ library/include/hipblas-common.h
@@ -80,11 +80,14 @@ typedef enum
     HIPBLAS_COMPUTE_32F_FAST_16BF = 5, /**< 32-bit input can is bf16 compute */
     HIPBLAS_COMPUTE_32F_FAST_TF32
     = 6, /**< 32-bit input can use tensor cores w/ TF32 compute. Only supported with cuBLAS backend currently */
-    HIPBLAS_COMPUTE_64F              = 7, /**< compute will be at least 64-bit precision */
-    HIPBLAS_COMPUTE_64F_PEDANTIC     = 8, /**< compute will be exactly 64-bit precision */
-    HIPBLAS_COMPUTE_32I              = 9, /**< compute will be at least 32-bit integer precision */
-    HIPBLAS_COMPUTE_32I_PEDANTIC     = 10, /**< compute will be exactly 32-bit integer precision */
-    HIPBLAS_COMPUTE_32F_FAST_8F_FNUZ = 100, /**< 32-bit compute using fp8 mfma instruction */
+    HIPBLAS_COMPUTE_64F                 = 7, /**< compute will be at least 64-bit precision */
+    HIPBLAS_COMPUTE_64F_PEDANTIC        = 8, /**< compute will be exactly 64-bit precision */
+    HIPBLAS_COMPUTE_32I                 = 9, /**< compute will be at least 32-bit integer precision */
+    HIPBLAS_COMPUTE_32I_PEDANTIC        = 10, /**< compute will be exactly 32-bit integer precision */
+    HIPBLAS_COMPUTE_32F_FAST_8F_FNUZ    = 100, /**< 32-bit compute using fp8 mfma instruction */
+    HIPBLAS_COMPUTE_32F_FAST_8BF_FNUZ   = 101, /**< 32-bit compute using bf8 mfma instruction */
+    HIPBLAS_COMPUTE_32F_FAST_8F8BF_FNUZ = 102, /**< 32-bit compute using f8bf8 mfma instruction */
+    HIPBLAS_COMPUTE_32F_FAST_8BF8F_FNUZ = 103, /**< 32-bit compute using bf8f8 mfma instruction */
 } hipblasComputeType_t;
 
 #endif
