f26087966c2f211640eff850c49e2bcb80d36eb9
David Dixon <david.dixon@amd.com>
2025-04-22 23:13:39 +0000

    Copyright clean up and new lines at eof
    [ROCm/hipBLAS-common commit: df68e8cf8bba72e8fd802f734a4645371b61aba8]

--- cmake/hipblas-common-config.cmake
+++ cmake/hipblas-common-config.cmake
@@ -1,4 +1,7 @@
 # ########################################################################
+#
+# MIT License
+#
 # Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
